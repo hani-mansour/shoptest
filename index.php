d8096ed6cc8936a49a5bd982dcde10cf7074cd15
hani-mansour <hani.mansour.1990@gmail.com>
2019-10-03 13:48:34 +0200

    shop

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Mein Shop</title>
+    </head>
+    <body>
+        <h1>Willkommen in Shop</h1>
+        <ul>
+        <?php
+            $json = file_get_contents('http://product-service');
+            $obj = json_decode($json);
+            $products = $obj->products;
+            foreach($products as $product){
+                echo "<li>$product</li>";
+            }
+        ?>
+        </ul>
+    </body>
+</html>
